4d7e4c4a317fe6c1cc26ca4b607c7afeaf594c45
mateus <you@example.com>
2025-09-19 16:53:18 +0200

    changes in text university and history; add new page for history; add slider and background with basel images

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, university
+from .views import home, history, university
 
 urlpatterns = [
     path("", home, name="home"),
     path("university/", university, name="university"),
+    path("history/", history, name="history"),
 ]
--- core/views.py
+++ core/views.py
@@ -2,20 +2,18 @@ from django.shortcuts import render
 
 def home(request):
     context = {
-        "basel_history": (
-            "Basel, located on the Rhine at the tripoint of Switzerland, France, and Germany, "
-            "has a documented history dating back to the Roman era. It became a significant "
-            "medieval trade and cultural center, later flourishing with humanism and printing."
-        )
+        "carousel_images": [f"img/basel_images/image{i}.jpg" for i in range(1, 6)],
     }
     return render(request, "core/home.html", context)
 
+def history(request):
+    context = {
+        
+    }
+    return render(request, "core/history.html", context)
+
 def university(request):
     context = {
-        "uni_history": (
-            "The University of Basel (founded 1460) is Switzerland’s oldest university. "
-            "It has been home to notable scholars across theology, medicine, and the humanities, "
-            "and today is a leading research university with strong ties to life sciences."
-        )
+        
     }
     return render(request, "core/university.html", context)
